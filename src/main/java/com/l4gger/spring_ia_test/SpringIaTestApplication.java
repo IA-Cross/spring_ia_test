4806038652dcb6073af8925f463e894c2f9fd025
IA-Cross <ayanegui.isaac@gmail.com>
2025-03-18 21:12:07 -0600

    first commit

---
+++ src/main/java/com/l4gger/spring_ia_test/SpringIaTestApplication.java
@@ -0,0 +1,13 @@
+package com.l4gger.spring_ia_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/l4gger/spring_ia_test/SpringIaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l4gger.spring_ia_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
